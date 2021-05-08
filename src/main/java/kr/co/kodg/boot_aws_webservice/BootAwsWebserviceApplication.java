45c6f2d9a91b10267c5ed2eeb4bcc08f37da9acc
dgko94 <llkk1346@naver.com>
2021-05-08 18:51:30 +0900

    Initial commit

---
+++ src/main/java/kr/co/kodg/boot_aws_webservice/BootAwsWebserviceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kodg.boot_aws_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAwsWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAwsWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/kodg/boot_aws_webservice/BootAwsWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kodg.boot_aws_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAwsWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
